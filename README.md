bbe864738ab791df973c84621be3e4a03735fa24
Beedeeboom <stephanie.duffieux@gmail.com>
2020-10-10 15:40:33 +1000

    added updates to Readme

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Pig Game
 
-GAME RULES:
+[Click Here to Play Game](https://beedeeboom.github.io/pig-game/)
+
+![Pig Game](src/img/pig-game.png)
+
+### GAME RULES:
 
 - The game has 2 players, Player 1 starts the game by rolling the dice.
 - A player can roll the dice as many times as they wish. Each roll gets added to the player's CURRENT score until the player clicks hold. When the player clicks hold the total CURRENT score gets added to their global score. After that, it's the next player's turn
 - BUT, if the player rolls a 1, they lose their total CURRENT score and the game moves to the next player.
 - The first player to reach 100 points on their GLOBAL score wins the game!
 
-Have Fun :)
+### Have Fun! 
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,3 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
 var scores, roundScore, activePlayer, gamePlaying;
 
 function initGame() {
@@ -34,7 +24,7 @@ function initGame() {
 
 initGame();
 
-document.querySelector('.btn-roll').addEventListener('click', function() { //anonymous function
+document.querySelector('.btn-roll').addEventListener('click', function() { 
 	if(gamePlaying) {
 		//Random number
 		var dice = Math.floor(Math.random() * 6) + 1;
@@ -61,7 +51,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 		//Update UI 
 		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 		//Check if player won the game
-		if (scores[activePlayer] >= 20) {
+		if (scores[activePlayer] >= 100) {
 			document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
 			document.querySelector('.dice').style.display = 'none';
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
